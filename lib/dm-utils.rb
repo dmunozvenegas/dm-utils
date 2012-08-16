ac0a22a684cbb992bc2d48c7f5ef8b390e6d5c2b
Sandro Gomez <sandro.gomez@gmail.com>
2012-08-16 19:29:53 -0400

    build

--- lib/dm-utils.rb
+++ lib/dm-utils.rb
@@ -2,10 +2,17 @@ require 'dm-core'
 
 module DataMapper
   	module Utils
-   		# => Recibe un arreglo
-	    def self.constant_name(const , value)
-	       self.class_eval{ const_get(const) }[value - 1].to_s.capitalize
+   	
+	   	def self.included(model)
+	      model.extend ClassMethods
 	    end
-	end
 
+	    	module ClassMethods
+	   			def constant_name(const , value)
+	      			.class_eval{ const_get(const) }[value - 1].to_s.capitalize
+	   			end
+
+	  		end
+	  	Model.append_inclusions self
+	  	end
 end

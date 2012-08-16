12b5633a41c8a38a4d41c9b30f42e4068c745bae
Sandro Gomez <sandro.gomez@gmail.com>
2012-08-16 18:57:22 -0400

    namespace error fixed

--- lib/dm-utils.rb
+++ lib/dm-utils.rb
@@ -1,12 +1,11 @@
 require 'dm-core'
 
 module DataMapper
-  module Model
   	module Utils
    		# => Recibe un arreglo
 	    def self.constant_name(const , value)
 	       self.class_eval{ const_get(const) }[value - 1].to_s.capitalize
 	    end
 	end
-  end
+
 end

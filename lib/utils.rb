f402b4093257adede9bff9443767700343748388
Sandro Gomez <sandro.gomez@gmail.com>
2012-08-16 19:13:37 -0400

    fixed

--- lib/utils.rb
+++ lib/utils.rb
@@ -2,9 +2,13 @@ require 'dm-core'
 
 module DataMapper
   	module Utils
-   		# => Recibe un arreglo
-	    def self.constant_name(const , value)
-	       self.class_eval{ const_get(const) }[value - 1].to_s.capitalize
+  		def self.included(base)
+            base.class_eval do
+   			# => Recibe un arreglo
+	    		def self.constant_name(const , value)
+	       		self.class_eval{ const_get(const) }[value - 1].to_s.capitalize
+	    		end
+	    	end
 	    end
 	end
   end
